a2522469ac8d954f59e645553aeb9bfcaeb49278
Lucas Branisso <LUCASB@amdocs.com>
2018-11-07 11:01:09 -0200

    First commit, template project from Spring Initializr

---
+++ src/test/java/com/lucas/learningspringboot/LearningSpringBootSocialAppChat/LearningSpringBootSocialAppChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucas.learningspringboot.LearningSpringBootSocialAppChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningSpringBootSocialAppChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
