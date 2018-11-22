5cc8910a2adbfe9f744729627a7bef6593152188
Lucas Branisso <LUCASB@amdocs.com>
2018-11-22 19:08:22 -0200

    OutboundChatService: now publishes comments to clients

--- src/main/java/com/lucas/learningspringboot/LearningSpringBootSocialAppChat/OutboundChatService.java
+++ src/main/java/com/lucas/learningspringboot/LearningSpringBootSocialAppChat/OutboundChatService.java
@@ -1,5 +1,7 @@
 package com.lucas.learningspringboot.LearningSpringBootSocialAppChat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
@@ -7,19 +9,39 @@ import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
 
 @Service
 @EnableBinding(ChatServiceStreams.class)
 public class OutboundChatService  implements WebSocketHandler {
 	
+	private final static Logger log = LoggerFactory.getLogger(OutboundChatService.class);
+	
+	private Flux<String> flux;
+	private FluxSink<String> chatMessageSink;
+	
+	public OutboundChatService() {
+		this.flux = Flux.<String>create(
+				emitter -> this.chatMessageSink = emitter, FluxSink.OverflowStrategy.IGNORE)
+			.publish()
+			.autoConnect();
+	}
+	
 	@StreamListener(ChatServiceStreams.BROKER_TO_CLIENT)
 	public void listen(String message) {
-		
+		if (chatMessageSink != null) {
+			log.info("Publishing " + message + " to web socket.");
+			chatMessageSink.next(message);
+		}
 	}
 	
 	@Override
 	public Mono<Void> handle(WebSocketSession session) {
-		return Mono.empty();
+		return session.send(
+				this.flux
+					.map(session::textMessage)
+					.log("Outbound: Wrap as websocket message"))
+				.log("Outbound: Publish to websocket");
 	}
 }
--- src/test/java/com/lucas/learningspringboot/LearningSpringBootSocialAppChat/OutboundChatServiceIntegrationTests.java
+++ src/test/java/com/lucas/learningspringboot/LearningSpringBootSocialAppChat/OutboundChatServiceIntegrationTests.java
@@ -4,9 +4,23 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.net.URI;
 import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Consumer;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -27,11 +41,24 @@ import reactor.core.publisher.Mono;
 @ActiveProfiles("test")
 public class OutboundChatServiceIntegrationTests {
 	
+	private static final String MESSAGE = "Hello";
+
 	@Autowired
 	ChatServiceStreams chatServiceStreams;
 	
 	@LocalServerPort
 	private int port;
+	
+	@Rule
+	public TestRule watcher = new TestWatcher() {
+		protected void starting(Description description) {
+			System.out.println("START - " + description.getMethodName());
+		}
+		
+		protected void finished(Description description) {
+			System.out.println("END - " + description.getMethodName());
+		}
+	};
 	
 	@Test
 	public void receivesMessageFromBroker() {
@@ -42,30 +69,47 @@ public class OutboundChatServiceIntegrationTests {
 	public void acceptsWebSocketConnectionFromClients() {
 		TestWebSocketHandler testWebSocketHandler = connectToOutboundChatService();
 		
-		assertThat(testWebSocketHandler.session).isNotNull();
+		assertThat(testWebSocketHandler.connected).isTrue();
+	}
+	
+	@Test
+	public void redirectsIncomingMessageFromBrokerToWebSocketClients() throws InterruptedException, ExecutionException, TimeoutException {
+		TestWebSocketHandler testWebSocketHandler = connectToOutboundChatService();
+		
+		sendMessage();
+
+		assertThat(testWebSocketHandler.receivedMessage.get(10, TimeUnit.SECONDS)).isEqualTo(MESSAGE);
 	}
 	
 	private boolean sendMessage() {
-		return chatServiceStreams.brokerToClient().send(MessageBuilder.withPayload("Hello").build());
+		return chatServiceStreams.brokerToClient().send(MessageBuilder.withPayload(MESSAGE).build());
 	}
 	
 	private TestWebSocketHandler connectToOutboundChatService() {
 		WebSocketClient client = new StandardWebSocketClient();
 		TestWebSocketHandler testWebSocketHandler = new TestWebSocketHandler();
 		
-		client.execute(URI.create("ws://localhost:" + port + "/topic/chatMessage.new"), testWebSocketHandler)
-			.block(Duration.ofSeconds(2));
+		try {
+			client.execute(URI.create("ws://localhost:" + port + "/topic/chatMessage.new"), testWebSocketHandler)
+			//.subscribe(aVoid -> System.out.println("Subscribed")); //Doesn't work
+			.block(Duration.ofSeconds(2));//Doesn't look good...
+		} catch (RuntimeException e) {
+			System.out.println("Ignoring timeout");
+		}
 		
 		return testWebSocketHandler;
 	}
 	
 	public class TestWebSocketHandler implements WebSocketHandler {
-		protected WebSocketSession session;
-		
+		boolean connected = false;
+		CompletableFuture<String> receivedMessage = new CompletableFuture<>();
 		@Override
 		public Mono<Void> handle(WebSocketSession session) {
-			this.session = session;
-			return Mono.empty();
+			connected = true;
+			return session.receive()
+				.map(message -> 
+					receivedMessage.complete(message.getPayloadAsText()))
+				.then();
 		}
 	}
 }
